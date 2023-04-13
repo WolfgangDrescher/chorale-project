599991548939fae9911065b5016140ffe726ae4e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-04-13 18:15:25 +0200

    Add create-yaml script for kittel

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,1 +1,2 @@
 import './bach-370-chorales/index.mjs';
+import './kittel-24-chorales/index.mjs';
---
+++ scripts/kittel-24-chorales/create-yaml.mjs
@@ -0,0 +1,40 @@
+import { execSync } from 'node:child_process';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { decode } from 'html-entities';
+import { getFiles, readFile } from '../utils/fs.mjs'; 
+import { parseHumdrumReferenceRecords } from '../utils/humdrum.mjs';
+import { writeYaml } from '../utils/yaml.mjs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function getIdFromFilePath(path) {
+    return path.split(/[\\\/]/).pop().split('.')[0];
+}
+
+const yamlPath = `${__dirname}/../../content/kittel-24-chorales`;
+
+execSync(`rm -rf ${yamlPath}/*`);
+execSync(`mkdir -p ${yamlPath}`);
+
+getFiles(`${__dirname}/../../kittel-24-chorales/kern`).forEach(file => {
+    const id = getIdFromFilePath(file);
+    console.log(id);
+    const yamlFile = `${yamlPath}/${id}.yaml`;
+    const kern = readFile(file);
+    const referenceRecords = parseHumdrumReferenceRecords(kern);
+
+    const chorale = {};
+    
+    chorale.id = id;
+
+    chorale.nr = parseInt(id.replace('chor', ''), 10);
+
+    chorale.title = decode(referenceRecords['OTL@@DE']) || null;
+
+    chorale.rawFile = `https://raw.githubusercontent.com/WolfgangDrescher/kittel-24-chorales/master/kern/${id}.krn`;
+
+    chorale.sourceFile = `https://github.com/WolfgangDrescher/kittel-24-chorales/blob/master/kern/${id}.krn`;
+
+    writeYaml(yamlFile, chorale);
+});
