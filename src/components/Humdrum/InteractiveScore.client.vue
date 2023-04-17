c8d14df1d70c89f9fe7b84b579087f28c7c21f03
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-04-17 12:58:27 +0200

    Add interactive score component

---
+++ src/components/Humdrum/InteractiveScore.client.vue
@@ -0,0 +1,66 @@
+<script setup>
+const props = defineProps({
+    url: {
+        type: String,
+        required: true,
+    },
+    verovioOptions: {
+        type: Object,
+        default: () => ({}),
+    },
+    scoreFormatter: {
+        type: Object,
+        required: true,
+    },
+});
+
+const emit = defineEmits([
+    'scoreIsReady',
+]);
+
+const verovioCanvas = ref(null);
+const { setData, formattedScoreData, filtersAsString } = props.scoreFormatter;
+
+const response = await fetch(props.url);
+if (!response.ok) {
+    throw createError({ statusCode: response.status, statusMessage: response.statusText });
+}
+setData(await response.text())
+
+const verovioCanvasOptions = computed(() => {
+    return Object.assign({
+        pageMargin: 50,
+        options: {
+            ...props.verovioOptions,
+            svgBoundingBoxes: true,
+            svgViewBox: true,
+        },
+        data: formattedScoreData.value,
+    });
+});
+
+let callVerovioMethod;
+
+function verovioCanvasScoreIsReady(event) {
+    callVerovioMethod = event.callVerovioMethod;
+    emit('scoreIsReady', event);
+}
+
+defineExpose({
+    verovioCanvas,
+});
+</script>
+
+<template>
+    <pre v-text="filtersAsString"></pre>
+    <div class="flex flex-col gap-4">
+        <div>
+            <div class="mt-4 flex flex-col gap-4">
+                <slot></slot>
+            </div>
+        </div>
+        <div>
+            <VerovioCanvas ref="verovioCanvas" v-bind="verovioCanvasOptions" @score-is-ready="verovioCanvasScoreIsReady" />
+        </div>
+    </div>
+</template>
