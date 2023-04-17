c1b5543efb38ab40d84da6b92e85e7dc1df3c84c
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-04-17 10:36:40 +0200

    Add Checkbox and Group component

---
+++ src/components/Form/Group.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(['label']);
+</script>
+
+<template>
+    <div class="flex flex-col w-full">
+        <div v-if="label || label === ''">
+            <label class="label-text text-gray-800 text-sm mb-2">{{ label }}</label>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.label-text:empty:before {
+    content: "\00a0";
+}
+</style>
