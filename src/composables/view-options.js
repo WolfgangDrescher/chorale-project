13519b5690cec6aaae71d5aaa78dd6e6ccaa07b1
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-04-17 12:55:45 +0200

    Add view options store

---
+++ src/composables/view-options.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia';
+
+const defaultFilter = {
+    scoreFilters: [],
+};
+
+export const useBachViewOptionsStore = defineStore('bach_view_options', {
+    state: () => ({ ...defaultFilter }),
+});
