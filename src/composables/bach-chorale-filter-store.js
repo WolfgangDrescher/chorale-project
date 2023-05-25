1f802c7beb73278e0a080137058b9a5f34c1202e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-05-25 11:59:25 +0200

    Add bach chorale filter pinia store

---
+++ src/composables/bach-chorale-filter-store.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+
+const defaultFilter = {
+    searchText: null,
+    keys: [],
+    majorMinor: null,
+};
+
+export const useBachChoraleFilterStore = defineStore('bach_chorale_filter', {
+    state: () => ({ ...defaultFilter }),
+    actions: {
+        update(prop, value) {
+            console.log(prop, value);
+            this[prop] = value;
+        },
+        reset() {
+            this.$patch({ ...defaultFilter });
+        },
+    },
+});
