289ac240471b9e319748fcc6a9c4d68f62ddeb38
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-11-09 16:23:09 +0100

    Add create-cadences.mjs script

---
+++ scripts/bach-370-chorales/create-cadences.mjs
@@ -0,0 +1,74 @@
+import { execSync } from 'node:child_process';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { decode } from 'html-entities';
+import { getFiles, readFile } from '../utils/fs.mjs'; 
+import { lineIsDataRecord, lineIsFermataEnd } from '../utils/humdrum.mjs'; 
+import yaml from 'js-yaml'; 
+import { writeYaml } from '../utils/yaml.mjs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function getIdFromFilePath(path) {
+    return path.split(/[\\\/]/).pop().split('.')[0];
+}
+
+const yamlPath = `${__dirname}/../../content/bach-cadences`;
+
+execSync(`rm -rf ${yamlPath}/*`);
+execSync(`mkdir -p ${yamlPath}`);
+
+function escapeShell(cmd) {
+    return '"' + cmd.replace(/(["$`\\])/g, '\\$1') + '"';
+}
+
+function getKey(id) {
+    const choraleYamlFile = `${__dirname}/../../content/bach-370-chorales/${id}.yaml`;
+    const config = yaml.load(readFile(choraleYamlFile));
+    return config.key;
+}
+
+function getCadenceDegree(finalis, cadenceUltima) {
+    const kern = `**kern	**kern
+${finalis.toUpperCase()}	${cadenceUltima.toLowerCase()}`;
+    const stdout = execSync(`echo ${escapeShell(kern)} | hint -l -d -c`).toString();
+    return parseInt(stdout.split('\n')[1], 10);
+}
+
+getFiles(`${__dirname}/../../bach-370-chorales/kern`).forEach(file => {
+    const id = getIdFromFilePath(file);
+    console.log(id);
+    const yamlFile = `${yamlPath}/${id}.yaml`;
+    const key = getKey(id);
+    const output = execSync(`cat ${__dirname}/../../bach-370-chorales/kern/${id}.krn | fb -cai | fb -acon3 | beat -ca`).toString();
+    const lines = output.split('\n');
+    const cadences = [];
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const tokens = line.split('\t');
+        if (lineIsDataRecord(line) && lineIsFermataEnd(lines, i, [0, 3, 5, 7])) {
+            const cadence = {
+                fb: tokens[2],
+                beat: parseFloat(tokens[9]),
+                degree: getCadenceDegree(key, tokens[0]),
+                voices: {
+                    soprano: {
+                        interval: tokens[8],
+                    },
+                    alto: {
+                        interval: tokens[6],
+                    },
+                    tenor: {
+                        interval: tokens[4],
+                    },
+                    bass: {
+                        interval: null,
+                    },
+                }
+            }
+            cadences.push(cadence);
+            writeYaml(`${yamlPath}/${id}.yaml`, cadences);
+        }
+    }
+});
--- scripts/utils/humdrum.mjs
+++ scripts/utils/humdrum.mjs
@@ -28,3 +28,20 @@ export function getHumdrumReferenceRecod(humdrum, key) {
     }
     return refs[key] || null;
 }
+
+export function lineIsDataRecord(line, includeNullToken = false) {
+    return !line.startsWith('!') && !line.startsWith('*') && !line.startsWith('=') && !(!includeNullToken && line === '.');
+}
+
+export function lineIsFermataEnd(lines, lineIndex, kernSpineIndices) {
+    const spineTokens = lines[lineIndex]?.split('\t');
+    const checkTokenForFermata = (kernSpineIndex) => {
+        return spineTokens[kernSpineIndex]?.includes(';') ?? false;
+    };
+    if (kernSpineIndices.every(checkTokenForFermata)) {
+        return true;
+    } else if (kernSpineIndices.some(checkTokenForFermata)) {
+        throw new Error('Fermata has notes on other lines');
+    }
+    return false;
+}
