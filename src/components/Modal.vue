3527bcca47fddaaf0df333abd2c0d989cdd0a4d2
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-11-14 23:40:26 +0100

    Add title slot

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -8,6 +8,9 @@ defineEmits(['close']);
             <div @click="$emit('close')" tabindex="-1" aria-hidden="true" class="overflow-y overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-screen max-h-full bg-black/80">
                 <div class="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-2xl max-h-full" @click.stop>
                     <div class="bg-white rounded shadow">
+                        <div v-if="title" class="border-b px-4 py-2 text-lg font-bold">
+                            {{ title }}
+                        </div>
                         <div class="p-4 space-y-4">
                             <slot></slot>
                         </div>
