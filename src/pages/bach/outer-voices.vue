c5655d5cfb1024bb495ba4f1b77e85e3449c088f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-11-15 00:37:12 +0100

    Update default interval mode

--- src/pages/bach/outer-voices.vue
+++ src/pages/bach/outer-voices.vue
@@ -21,7 +21,7 @@ const { filteredElements } = useBachChoraleFilter(chorales);
 const totalSlices = outerVoicesData.value.body.length;
 
 const options = reactive({
-    mode: 'fb', // interval, fb, hint
+    mode: 'intervals', // intervals, fb, hint
     beat: '', // 1, 0.5, 0.5!1, 0.25!0.5
     position: null, // phraseStart, phraseEnd, choraleStart, choraleEnd
     fb: [],
