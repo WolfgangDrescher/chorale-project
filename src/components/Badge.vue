8ae441ba5b6e9d958c47b0a277a0eedbcf4c4f7a
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-11-14 23:39:30 +0100

    Add badge component

---
+++ src/components/Badge.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="hover:bg-primary-600 hover:ring ring-primary-100 bg-primary-500 text-xs text-white rounded inline-block m-1 px-1 cursor-pointer">
+        <slot></slot>
+    </div>
+</template>
