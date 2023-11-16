630d15fd64c7d6f8a62cb231f304fc4c7a42afdb
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-11-17 00:42:42 +0100

    Add description list component

---
+++ src/components/DescriptionList.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    items: Array,
+});
+</script>
+
+<template>
+    <dl class="flex flex-wrap gap-4">
+        <template v-for="item in items">
+            <dt class="flex-grow flex-shrink-0 basis-1/6">
+                <slot name="term" :item="item">
+                    {{ item.term }}
+                </slot>
+            </dt>
+            <dd class="flex-grow flex-shrink basis-4/6">
+                <slot name="detail" :item="item">
+                    {{ item.detail }}
+                </slot>
+            </dd>
+        </template>
+    </dl>
+</template>
