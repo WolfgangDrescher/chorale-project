d120e3c9ccd7edcbae3b97880b5cad6b80a492d0
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-12-11 23:43:12 +0100

    Change to @nuxtjs/i18n module

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxtjs/google-fonts',
-        '@nuxtjs/i18n-edge',
+        '@nuxtjs/i18n',
         '@nuxt/content',
         '@pinia/nuxt',
         'nuxt-icon',
