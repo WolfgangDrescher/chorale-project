a48a180f59c91e294c2031a655ba1e28ce5a9f15
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-12-18 23:53:41 +0100

    Wrap elements in ref

--- src/composables/array-loader.js
+++ src/composables/array-loader.js
@@ -1,11 +1,12 @@
 export function useArrayLoader(elements, numberOfItemsToAdd = 10) {
     const items = ref([]);
-    watch(elements, () => {
+    const elRef = ref(elements);
+    watch(elRef, () => {
         items.value = [];
         addItems(numberOfItemsToAdd);
     });
     function addItems(num = numberOfItemsToAdd) {
-        items.value.push(...unref(elements).slice(items.value.length, items.value.length + num));
+        items.value.push(...unref(elRef).slice(items.value.length, items.value.length + num));
     }
     addItems(numberOfItemsToAdd);
     return {
