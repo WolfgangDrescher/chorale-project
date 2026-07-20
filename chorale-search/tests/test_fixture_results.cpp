1b365e6e32207acae7ef468dc308a3cedb637540
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-07-20 22:12:25 +0200

    Add test case deg_3_2_1_or_3_2_3_in_soprano

--- chorale-search/tests/test_fixture_results.cpp
+++ chorale-search/tests/test_fixture_results.cpp
@@ -42,4 +42,28 @@ TEST_CASE(deg_3_2_1) {
     CHECK_RESULT(results[7], "chor029", 4, "47", "50");
 }
 
+TEST_CASE(deg_3_2_1_or_3_2_3_in_soprano) {
+    Query q;
+    q.feature = "deg";
+    q.pattern = {
+        AttributeMap{{"deg", {"3"}}},
+        AttributeMap{{"deg", {"2"}}},
+        AttributeMap{{"deg", {"1", "3"}}},
+    };
+    q.voices = "soprano";
+
+    CorpusSearch search(FIXTURE_CHORALE("chor029"));
+    auto results = search.run(q);
+    std::cout << results;
+
+    REQUIRE(results.size() == 7u);
+
+    CHECK_RESULT(results[0], "chor029", 4, "5", "7");
+    CHECK_RESULT(results[1], "chor029", 4, "12", "14");
+    CHECK_RESULT(results[2], "chor029", 4, "20", "22");
+    CHECK_RESULT(results[3], "chor029", 4, "29", "32");
+    CHECK_RESULT(results[4], "chor029", 4, "35", "36");
+    CHECK_RESULT(results[5], "chor029", 4, "45", "47");
+    CHECK_RESULT(results[6], "chor029", 4, "47", "50");
+}
 TEST_MAIN()
