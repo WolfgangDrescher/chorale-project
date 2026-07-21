bb44b3897a3c6510a3f47490a94603935b8c88b2
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-07-21 13:13:02 +0200

    Add pinia to nuxt config

--- chorale-webapp/nuxt.config.ts
+++ chorale-webapp/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: false },
-    modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+    modules: ['@nuxt/ui', '@nuxtjs/i18n', '@pinia/nuxt'],
     css: ['~/assets/css/main.css'],
     i18n: {
         strategy: 'prefix_except_default',
