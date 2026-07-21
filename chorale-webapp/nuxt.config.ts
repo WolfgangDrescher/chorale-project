d95b58a56cd7746ef96332f44205cec4f610270e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-07-21 10:33:37 +0200

    Add default styles

--- chorale-webapp/nuxt.config.ts
+++ chorale-webapp/nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+    css: ['~/assets/css/main.css'],
 })
