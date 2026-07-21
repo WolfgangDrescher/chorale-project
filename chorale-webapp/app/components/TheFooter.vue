1a3ee240abecd6ca603eafb9c5cab47266e37762
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-07-21 10:35:22 +0200

    Add default layout

---
+++ chorale-webapp/app/components/TheFooter.vue
@@ -0,0 +1,51 @@
+<script setup>
+const localePath = useLocalePath();
+
+const { t } = useI18n();
+
+const columns = [
+    {
+        label: t('pages'),
+        children: [
+            {
+                label: t('home'),
+                to: localePath('index'),
+            },
+        ],
+    },
+    {
+        label: t('sitemap'),
+        children: [
+            {
+                label: t('legalNotice'),
+                to: localePath('legal-notice'),
+            },
+        ],
+    },
+];
+</script>
+
+<template>
+    <UFooter class="bg-pattern">
+        <template #top>
+            <UContainer>
+                <UFooterColumns :columns="columns" :ui="{
+                    list: 'grid grid-cols-2 gap-x-2 space-y-0 mt-3',
+                    link: 'text-white text-base',
+                    label: 'text-white text-lg',
+                }">
+                    <template #right>
+                        <div>
+                            <div class="flex justify-end">
+                                <img src="/mhfr-logo-white.svg" class="max-h-[100px]" alt="Hochschule für Musik Freiburg" />
+                            </div>
+                            <div class="flex justify-end">
+                                <img src="/fzm-logo-white.svg" class="max-h-[130px]" alt="FZM – Freiburger Forschungs- und Lehrzentrum Musik" />
+                            </div>
+                        </div>
+                    </template>
+                </UFooterColumns>
+            </UContainer>
+        </template>
+    </UFooter>
+</template>
---
+++ chorale-webapp/app/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<script setup>
+const localePath = useLocalePath();
+</script>
+
+<template>
+    <header class="bg-primary-500 text-white bg-pattern">
+        <div class="flex justify-between items-center py-2 px-4 gap-4">
+            <img src="/mhfr-logo-white.svg" class="hidden md:block max-h-[50px] mx-auto" alt="Hochschule für Musik Freiburg" />
+            <div class="grow flex items-center gap-4">
+                <div class="font-extrabold text-lg md:text-2xl">
+                    <NuxtLink :to="localePath('/')">
+                        Chorale Project
+                    </NuxtLink>
+                </div>
+            </div>
+            <div class="flex gap-2 justify-end items-center">
+                <UButton
+                    icon="i-simple-icons-github"
+                    to="https://github.com/WolfgangDrescher/chorale-project"
+                    target="_blank"
+                    variant="ghost"
+                    color="neutral"
+                    square
+                    :ui="{ label: 'hidden', base: 'text-white hover:text-black' }"
+                />
+            </div>
+        </div>
+    </header>
+</template>
